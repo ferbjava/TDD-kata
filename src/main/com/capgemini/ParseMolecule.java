fff76ba484f6268c810f7c257c9073a8173a0176
ferbjava <michal.kotecki@capgemini.com>
2018-10-12 13:54:23 +0200

    molecules update

--- src/main/com/capgemini/ParseMolecule.java
+++ src/main/com/capgemini/ParseMolecule.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class ParseMolecule {
 
-	private List<String> atoms = Arrays.asList("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al",
+	private static List<String> atoms = Arrays.asList("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al",
 			"Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga",
 			"Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In",
 			"Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Ti", "Pb",
@@ -18,9 +18,28 @@ public class ParseMolecule {
 
 	public static Map<String, Integer> getAtoms(String formula) {
 		List<String> str = Arrays.stream(formula.split("")).collect(Collectors.toList());
-		List<String> atoms = new ArrayList<>();
+		List<String> molecules = new ArrayList<>();
 		List<Integer> occur = new ArrayList<>();
-		List<Integer> brackets = new ArrayList<>();
+		List<Integer> leftBrackets = new ArrayList<>();
+		List<Integer> rightBrackets = new ArrayList<>();
+		Map<String, Integer> mol = new HashMap<>();
+		mol.put("O", 3);
+		mol.put("N", 2);
+		mol.put("Ca", 1);
+		System.out.println(mol.toString());
+		
+		for(int i = 0; i < str.size(); i++) {
+			String recentChar = str.get(i);
+			if(Character.isLetter(recentChar.toCharArray()[0])) {
+				String nextChar = str.get(i+1);
+				if(Character.isLetter(nextChar.toCharArray()[0])) {
+					String molecule = recentChar.concat(nextChar);
+					if(ParseMolecule.atoms.contains(molecule)) {
+						
+					}
+				}
+			}
+		}
 		
 		Iterator<String> stringIterator = str.iterator();
 		while(stringIterator.hasNext()){
