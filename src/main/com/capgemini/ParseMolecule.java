5dc73b815bb6fb809136b4df7b217c91f01573fd
ferbjava <michal.kotecki@capgemini.com>
2018-10-15 17:08:54 +0200

    Molecules updata

--- src/main/com/capgemini/ParseMolecule.java
+++ src/main/com/capgemini/ParseMolecule.java
@@ -10,58 +10,49 @@ import java.util.stream.Collectors;
 
 public class ParseMolecule {
 
-	private static List<String> atoms = Arrays.asList("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al",
-			"Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga",
-			"Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In",
-			"Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Ti", "Pb",
-			"Bi", "Po", "At", "Rn");
+	private static List<String> atoms = Arrays.asList("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
+			"Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+			"Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+			"In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Ti",
+			"Pb", "Bi", "Po", "At", "Rn");
 
 	public static Map<String, Integer> getAtoms(String formula) {
 		List<String> str = Arrays.stream(formula.split("")).collect(Collectors.toList());
 		List<String> molecules = new ArrayList<>();
-		List<Integer> occur = new ArrayList<>();
+		List<String> moleculesInBrackets = new ArrayList<>();
+		List<Integer> occurence = new ArrayList<>();
 		List<Integer> leftBrackets = new ArrayList<>();
 		List<Integer> rightBrackets = new ArrayList<>();
 		Map<String, Integer> mol = new HashMap<>();
-		mol.put("O", 3);
-		mol.put("N", 2);
-		mol.put("Ca", 1);
-		System.out.println(mol.toString());
+		// mol.put("O", 3);
+		// mol.put("N", 2);
+		// mol.compute("Ca", (k, v) -> v == null ? 1 : v + 1);
+		// mol.compute("N", (k, v) -> v == null ? 1 : v + 1);
+		// System.out.println(mol.toString());
+
+		List<String[]> stringsArray = new ArrayList<>();
 		
-		for(int i = 0; i < str.size(); i++) {
+		String molecule = "";
+		Integer occur = 0;
+		for (int i = 0; i < str.size(); i++) {
 			String recentChar = str.get(i);
-			if(Character.isLetter(recentChar.toCharArray()[0])) {
-				String nextChar = str.get(i+1);
-				if(Character.isLetter(nextChar.toCharArray()[0])) {
-					String molecule = recentChar.concat(nextChar);
-					if(ParseMolecule.atoms.contains(molecule)) {
-						
+			if (Character.isLetter(recentChar.toCharArray()[0])) {
+				String nextChar = str.get(i + 1);
+				if (Character.isLowerCase(nextChar.toCharArray()[0])) {
+					molecule = recentChar.concat(nextChar);
+					if (!ParseMolecule.atoms.contains(molecule)) {
+						throw new IllegalArgumentException();
 					}
+				} else {
+					molecule = recentChar;
 				}
+			} else if (Character.isDigit(recentChar.toCharArray()[0])) {
+
+			} else if (recentChar.matches("(|[|]|)")) {
+
 			}
 		}
-		
-		Iterator<String> stringIterator = str.iterator();
-		while(stringIterator.hasNext()){
-			String recentChar = stringIterator.next();
-			if(Character.isLetter(recentChar.toCharArray()[0])) {
-				String nextChar = stringIterator.next();
-			}
-		}
-		
-		for(String s: str){
-			if(Character.isLetter(s.toCharArray()[0])){
-				
-			}
-		}
-		
-		char[] chars = formula.toCharArray();
-		for (int i = 0; i < chars.length; i++) {
-			if (Character.isLetter(chars[i])) {
-				
-			}
-		}
-		
+
 		return new HashMap<String, Integer>();
 	}
 }
