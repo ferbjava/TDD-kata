b374d714a95a570a872705d6a96e7f1517a9f021
ferbjava <michal.kotecki@capgemini.com>
2018-10-19 15:33:14 +0200

    cd...

--- src/main/com/capgemini/ParseMolecule.java
+++ src/main/com/capgemini/ParseMolecule.java
@@ -3,7 +3,6 @@ package com.capgemini;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,43 +15,66 @@ public class ParseMolecule {
 			"In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Ti",
 			"Pb", "Bi", "Po", "At", "Rn");
 
+	private static Map<String, Integer> mol = new HashMap<String, Integer>();
+
 	public static Map<String, Integer> getAtoms(String formula) {
+
 		List<String> str = Arrays.stream(formula.split("")).collect(Collectors.toList());
+
 		List<String> molecules = new ArrayList<>();
 		List<String> moleculesInBrackets = new ArrayList<>();
 		List<Integer> occurence = new ArrayList<>();
 		List<Integer> leftBrackets = new ArrayList<>();
 		List<Integer> rightBrackets = new ArrayList<>();
 		Map<String, Integer> mol = new HashMap<>();
+
 		// mol.put("O", 3);
 		// mol.put("N", 2);
 		// mol.compute("Ca", (k, v) -> v == null ? 1 : v + 1);
 		// mol.compute("N", (k, v) -> v == null ? 1 : v + 1);
 		// System.out.println(mol.toString());
 
-		List<String[]> stringsArray = new ArrayList<>();
-		
+		List<List<String>> globalList = new ArrayList<>();
+		List<String> localList = new ArrayList<>();
+
 		String molecule = "";
+		String recentMolecule = "";
 		Integer occur = 0;
 		for (int i = 0; i < str.size(); i++) {
 			String recentChar = str.get(i);
-			if (Character.isLetter(recentChar.toCharArray()[0])) {
-				String nextChar = str.get(i + 1);
-				if (Character.isLowerCase(nextChar.toCharArray()[0])) {
-					molecule = recentChar.concat(nextChar);
-					if (!ParseMolecule.atoms.contains(molecule)) {
-						throw new IllegalArgumentException();
-					}
-				} else {
-					molecule = recentChar;
-				}
+
+			if (Character.isUpperCase(recentChar.toCharArray()[0])) {
+				verifyMolecule(recentMolecule);
+				localList.add(recentMolecule);
+				recentMolecule = recentChar;
+			} else if (Character.isLowerCase(recentChar.toCharArray()[0])) {
+				recentMolecule = recentMolecule.concat(recentChar);
+				verifyMolecule(recentMolecule);
+				localList.add(recentMolecule);
+
 			} else if (Character.isDigit(recentChar.toCharArray()[0])) {
+				globalList.add(localList);
+				localList.clear();
+				recentMolecule = "";
+				occur = occur * 10 + Integer.parseInt(recentChar);
+			} else if (recentChar.matches("(|[")) {
 
-			} else if (recentChar.matches("(|[|]|)")) {
-
+			} else if (recentChar.matches(")|]")) {
+				globalList.add(localList);
+				localList.clear();
+			} else {
+				throw new IllegalArgumentException();
 			}
+
 		}
 
-		return new HashMap<String, Integer>();
+		return ParseMolecule.mol;
+//		return new HashMap<String, Integer>();
+	}
+
+	private static void verifyMolecule(String molecule) {
+		if (!molecule.equals("") && !ParseMolecule.atoms.contains(molecule)) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
